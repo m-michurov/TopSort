da574d379816ebca67e8b69aadd64e8663f314ee
m-michurov <>
2019-02-26 20:55:54 +0700

    Initial commit

---
+++ cmake-build-debug/CMakeFiles/TopSort.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/TopSort.dir/main.c.obj"
+  "TopSort.pdb"
+  "TopSort.exe"
+  "TopSort.exe.manifest"
+  "libTopSort.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/TopSort.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ definitions.h
@@ -0,0 +1,23 @@
+#pragma  once
+
+#ifndef TOPSORT_DEFINITIONS_H
+#define TOPSORT_DEFINITIONS_H
+
+#include <stdio.h>
+
+#define MEM_ERROR           {puts("Error: unable to allocate memory");  exit(EXIT_SUCCESS);};
+#define SORT_ERROR          {puts("impossible to sort");                exit(EXIT_SUCCESS);};
+#define VERTEX_NUM_ERROR      {puts("bad number of vertices");            exit(EXIT_SUCCESS);};
+#define EDGE_NUM_ERROR      {puts("bad number of edges");               exit(EXIT_SUCCESS);};
+#define LINE_NUM_ERROR      {puts("bad number of lines");               exit(EXIT_SUCCESS);};
+#define VERTEX_INPUT_ERROR    {puts("bad vertex");                        exit(EXIT_SUCCESS);};
+
+
+
+typedef enum {
+    WHITE,
+    GREY,
+    BLACK
+} COLOUR;
+
+#endif
